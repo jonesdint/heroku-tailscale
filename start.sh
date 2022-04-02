22c2b8e8330eace884bb9b57bd2631f4a1bf28ca
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-04-02 18:50:31 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app --advertise-exit-node
+do
+    sleep 2
+done
+echo Tailscale started
+ALL_PROXY=socks5://localhost:1055/
+while true; do foo; sleep 100; done

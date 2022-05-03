b0eb9727885699f60bd1d9d97d31f01fc48a0dad
Ryan <rdotts@gmail.com>
2022-05-03 12:21:23 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,9 @@
 #!/bin/sh
 
-
-# refer to https://tailscale.com/kb/1107/heroku/
-
-
-
-#/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 & \
-#/app/tailscale down & \
-#/app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=heroku-app --advertise-exit-node & \
-#/app/tailscale status
-
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=heroku-app --advertise-exit-node
+until /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=heroku-app --accept-routes --accept-dns
 do
     sleep 30
 done
 
-echo Tailscale started
-
 ALL_PROXY=socks5://localhost:1055/
-
-sleep infinity
-
-#while true; do foo; sleep 100; done
-
-#https://forum.tailscale.com/t/create-exit-node-on-railway-app/1837/4

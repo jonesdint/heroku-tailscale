93d74c240d6982832dfe09278ffa5ec9dcbac919
Ryan Dotts <rdotts@gmail.com>
2022-05-03 17:07:53 -0400

    Customized settings.js

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=heroku-app --accept-routes --accept-dns
+until /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=dotts-heroku --accept-routes --accept-dns
 do
     sleep 30
 done
 
-ALL_PROXY=socks5://localhost:1055/
+export ALL_PROXY=socks5://localhost:1055/

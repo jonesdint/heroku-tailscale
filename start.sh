10dedc95d387436225549f90af915af969ac5e11
David Jones <david@intelliware.ca>
2023-06-27 12:35:40 -0400

    Update names

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=dotts-heroku --accept-routes --accept-dns
+until /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=heroku --accept-routes --accept-dns
 do
     sleep 30
 done
